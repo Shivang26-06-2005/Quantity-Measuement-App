0863e3f8ff8b70a18fa8f395e24e2c00d7fc3602
Solgreta26 <78587983+Shivang26-06-2005@users.noreply.github.com>
2026-04-24 09:37:05 +0530

    UC4 Done

--- app/src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ app/src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,66 +2,43 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-<<<<<<< Updated upstream
-    // Inner class to represent Feet measurement
-    public static class Feet {
-        private final double value;
+    public static void demonstrateLengthComparison(
+            double value1, Length.LengthUnit unit1,
+            double value2, Length.LengthUnit unit2) {
 
-        // Constructor
-        public Feet(double value) {
-            this.value = value;
-        }
+        Length l1 = new Length(value1, unit1);
+        Length l2 = new Length(value2, unit2);
 
-        /**
-         * Override equals() method to compare two Feet objects based on their value
-         */
-        @Override
-        public boolean equals(Object obj) {
-
-            // 1. Reference check
-            if (this == obj) return true;
-
-            // 2. Null check
-            if (obj == null) return false;
-
-            // 3. Type check
-            if (getClass() != obj.getClass()) return false;
-
-            // 4. Cast and compare values
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-
-        // Always override hashCode when equals is overridden
-        @Override
-        public int hashCode() {
-            return Double.hashCode(value);
-        }
+        System.out.println("Input: Quantity(" + value1 + ", " + unit1 +
+                ") and Quantity(" + value2 + ", " + unit2 + ")");
+        System.out.println("Output: Equal (" + l1.equals(l2) + ")");
+        System.out.println();
     }
 
-    // Main method to demonstrate functionality
-    public static void main(String[] args) {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
-
-        System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal (" + f1.equals(f2) + ")");
-=======
     public static void main(String[] args) {
 
-        // ===== Cross-unit comparison =====
-        Length l1 = new Length(1.0, Length.LengthUnit.FEET);
-        Length l2 = new Length(12.0, Length.LengthUnit.INCHES);
+        // Feet ↔ Inches
+        demonstrateLengthComparison(1.0, Length.LengthUnit.FEET,
+                12.0, Length.LengthUnit.INCHES);
 
-        System.out.println("Input: Quantity(1.0, \"feet\") and Quantity(12.0, \"inches\")");
-        System.out.println("Output: Equal (" + l1.equals(l2) + ")");
+        // Yards ↔ Feet
+        demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS,
+                3.0, Length.LengthUnit.FEET);
 
-        // ===== Same unit comparison =====
-        Length l3 = new Length(1.0, Length.LengthUnit.INCHES);
-        Length l4 = new Length(1.0, Length.LengthUnit.INCHES);
+        // Yards ↔ Inches
+        demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS,
+                36.0, Length.LengthUnit.INCHES);
 
-        System.out.println("Input: Quantity(1.0, \"inch\") and Quantity(1.0, \"inch\")");
-        System.out.println("Output: Equal (" + l3.equals(l4) + ")");
->>>>>>> Stashed changes
+        // CM ↔ Inches
+        demonstrateLengthComparison(1.0, Length.LengthUnit.CENTIMETERS,
+                0.393701, Length.LengthUnit.INCHES);
+
+        // Feet ↔ Yards
+        demonstrateLengthComparison(3.0, Length.LengthUnit.FEET,
+                1.0, Length.LengthUnit.YARDS);
+
+        // CM ↔ Feet
+        demonstrateLengthComparison(30.48, Length.LengthUnit.CENTIMETERS,
+                1.0, Length.LengthUnit.FEET);
     }
 }
